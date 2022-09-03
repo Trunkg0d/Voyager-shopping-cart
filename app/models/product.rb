112f64d8371662a6ff4f4fc9a939541a71dfbb0c
Trunkg0d <trungsanglong@gmail.com>
2022-09-03 13:00:13 +0700

    Finish update avatar and product-image

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ApplicationRecord
     has_many :product_categories
     has_many :categories, through: :product_categories
     default_scope -> {order(created_at: :desc)}
-    has_one_attached :image
+    mount_uploader :image, ImageUploader
 end
