5d7ea3ad115b170ad6e9fa7306aa6927a0e4ad19
Trunkg0d <trungsanglong@gmail.com>
2022-09-07 09:50:13 +0700

    Add order and order_item

---
+++ db/migrate/20220907021930_create_orders.rb
@@ -0,0 +1,9 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.references :user
+      t.integer :sum_money
+      t.timestamps
+    end
+  end
+end
