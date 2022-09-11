0bf7c37951d3f32306c4776a0fadf84264f3d47d
Trunkg0d <trungsanglong@gmail.com>
2022-09-11 23:30:29 +0700

    Fix s3

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,11 +6,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [100,100]
   end
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production? || true
-    storage :fog
-  else
-    storage :file
-  end
+  # if Rails.env.production? || true
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,11 +13,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  if Rails.env.production? || true
-    storage :fog
-  else
-    storage :file
-  end
+  # if Rails.env.production? || true
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
