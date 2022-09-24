123b51d4dcfcaf0b30cf4bc8f508fd72b2210ce5
Trunkg0d <trungsanglong@gmail.com>
2022-09-24 20:47:15 +0700

    Add validates for shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,6 @@
 class Shop < ApplicationRecord
     belongs_to :user
     has_many :products, dependent: :destroy
+    validates :name, presence: true
+    validates :description, presence: true
 end
