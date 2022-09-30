a9516f9dc0d7160a00cf242700d1602c13b01e23
Trunkg0d <trungsanglong@gmail.com>
2022-09-30 21:40:52 +0700

    Show order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,8 @@ class OrdersController < ApplicationController
         #User order to sort by created time
         @orders = current_user.orders.order("created_at DESC") 
     end
+
+    def show
+        @order = Order.find(params[:id])
+    end
 end
