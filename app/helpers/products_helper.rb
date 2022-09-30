5d0bd1e0bee0d80152dce7d72bdc7efb1f48d474
Trunkg0d <trungsanglong@gmail.com>
2022-09-30 12:49:13 +0700

    Finish filter action

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -32,4 +32,51 @@ module ProductsHelper
         end
         return false
     end
+
+    def size_filter(product, size)
+        if size.nil?
+            return true
+        else
+            product.size_ids.each do |size_id|
+                if size[:commands].include?(Size.find(size_id).name)
+                    return true
+                    break
+                end
+            end 
+            return false
+        end
+    end
+
+    def brand_filter(product, brand)
+        if brand.nil?
+            return true
+        else
+            if brand[:commands].include?(product.shop.name)
+                return true
+            else
+                return false
+            end
+        end
+    end
+
+    def category_filter(product, category)
+        if category.nil?
+            return true
+        else
+            product.category_ids.each do |category_id|
+                if category[:commands].include?(Category.find(category_id).name)
+                    return true
+                    break
+                end
+            end 
+            return false
+        end
+    end
+
+    def filter(product, brand, size, category)
+        if brand.nil? && size.nil? && category.nil?
+            return false
+        end
+        return brand_filter(product, brand) && size_filter(product, size) && category_filter(product, category)
+    end
 end
