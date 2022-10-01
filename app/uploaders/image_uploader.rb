079462d2846e88c649e27634770310557753da0c
Trunkg0d <trungsanglong@gmail.com>
2022-10-01 23:25:11 +0700

    Turn of s3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,13 +12,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  storage :file
 
-  if Rails.env.production? 
-    storage :fog
-  else
-    storage :file
-  end
+  # if Rails.env.production? 
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
