ebe2b308af09f478b5ac67473af87dc644dd9cb1
Trunkg0d <trungsanglong@gmail.com>
2022-10-01 23:00:31 +0700

    Add s3

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,13 +6,13 @@ class AvatarUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [100,100]
   end
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
 
-  # if Rails.env.production? 
-  #   storage :fog
-  # else
-  #   storage :file
-  # end
+  if Rails.env.production? 
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
