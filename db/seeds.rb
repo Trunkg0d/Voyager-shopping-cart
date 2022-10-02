f31a9ecfe783873c7d7a5e8c3c4fe1db11119375
Trunkg0d <trungsanglong@gmail.com>
2022-10-02 14:12:43 +0700

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -85,10 +85,10 @@ followers.each { |follower| follower.follow(user) }
 
 users = User.all
 users.each do |user|
-    user.shop.products.create!(name: "Nike KD 15", price: 150, description:"Limited edition",public: 1, sale:1, size_ids:[5,7,10],color_ids:[1,13], images: [File.open(File.join(Rails.root, 'app', 'assets','images', 'KD15', 'KD15.webp'))])
-    user.shop.products.create!(name: "Nike Air Force 1", price: 150, description:"Limited edition",public: 1, sale: 1, size_ids:[5,7,10],color_ids:[1,13], images: [File.open(File.join(Rails.root, 'app', 'assets','images', 'NikeAirForce1', 'NikeAirForce1.webp'))])
-    user.shop.products.create!(name: "Nike Air Max 90", price: 150, description:"Limited edition",public: 1, size_ids:[5,7,10],color_ids:[1,13], images: [File.open(File.join(Rails.root, 'app', 'assets','images', 'NikeAirMax90', 'NikeAirMax90.webp'))])
-    user.shop.products.create!(name: "Nike Infinity", price: 150, description:"Limited edition",public: 1, size_ids:[5,7,10],color_ids:[1,13], images: [File.open(File.join(Rails.root, 'app', 'assets','images', 'NikeInfinityRun3', 'NikeInfinityRun3.webp'))]) 
+    user.shop.products.create!(name: "Nike KD 15", price: 150, description:"Limited edition",public: 1, sale:1, size_ids:[10],color_ids:[1,13], images: [File.open(File.join(Rails.root, 'app', 'assets','images', 'KD15', 'KD15.webp'))])
+    user.shop.products.create!(name: "Nike Air Force 1", price: 150, description:"Limited edition",public: 1, sale: 1, size_ids:[10],color_ids:[1,13], images: [File.open(File.join(Rails.root, 'app', 'assets','images', 'NikeAirForce1', 'NikeAirForce1.webp'))])
+    user.shop.products.create!(name: "Nike Air Max 90", price: 150, description:"Limited edition",public: 1, size_ids:[10],color_ids:[1,13], images: [File.open(File.join(Rails.root, 'app', 'assets','images', 'NikeAirMax90', 'NikeAirMax90.webp'))])
+    user.shop.products.create!(name: "Nike Infinity", price: 150, description:"Limited edition",public: 1, size_ids:[10],color_ids:[1,13], images: [File.open(File.join(Rails.root, 'app', 'assets','images', 'NikeInfinityRun3', 'NikeInfinityRun3.webp'))]) 
 end
 
 User.create!(name: "Example User",
